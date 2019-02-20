aaf26b1b7a3e3bba8eaed255bdb398e2d090f645
mgrvp <mohgp855@gmail.com>
2019-02-20 22:18:19 +0700

    tambah gambar

--- app/src/main/java/com/example/putraprima/myapplication/MainActivity.java
+++ app/src/main/java/com/example/putraprima/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.putraprima.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 
     @Override
